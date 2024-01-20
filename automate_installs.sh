78d562bc2fc6513179d384ef97bb4a89fd95b2ce
zweatshirt <>
2024-01-19 19:37:30 -0600

    changes

--- automate_installs.sh
+++ automate_installs.sh
@@ -1,6 +1,15 @@
 #usr/bin/env bash
 
-echo "Installing conda"
+# Future implementation:
+# echo "Enter the directory to send this script to, 
+#so it can be executed (e.g. zlinsco@pc533.emulab.net:users/zlinsco):"
+#read expdir
+
+#scp "$0" "expdir"
+
+
+# Install miniconda
+echo "Installing miniconda"
 mkdir -p ~/miniconda3
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
@@ -12,22 +21,45 @@ rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 ~/miniconda3/bin/conda init zsh
 
-echo "Enter the name for the conda env"
+# Ask user for environment name they want
+echo "Enter the name for the conda env:"
 read name
 
+
 conda create --name $name python=3.11
+conda init bash
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate $name
 
+
+# Update, htop, and screen install
 echo "Updating... Additionally, installing htop and screen"
 sudo apt-get update
 sudo install htop
 sudo install screen
 
-echo "Time to install jupyter notebook"
+
+# Jupyter Notebook install
+echo "Time to install jupyter notebook."
 conda install -c conda-forge notebook
 conda install -c conda-forge nb_conda_kernels
 conda install nb_conda
 
+
+# Prompt user if they want to open Notebook
+echo "Open Jupyter Notebook? [y/n]"
+read ans
+if [ "$ans" == "${ans#[Yy]}" ]:
+then
+	readarray -t lines < <(jupyter notebook --no-browser --port=5900)
+	for line in "${lines[@]}"; do
+		if printf '%s\0' "${lines[@]}" | grep -Fxqz -- 'http://localhost:5900';
+		then
+			jup_link=$"{lines[@]}"
+			echo $jup_link
+else
+	echo "Cool, see ya later then."
+fi
 
 
 
